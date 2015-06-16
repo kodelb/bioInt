3d58b9ac2594819ebdf59a7b8781dede5af94c67
kodelb <boris.kodel@gmail.com>
2015-06-16 23:31:31 +0300

    added validation

--- src/main/java/bioInt/test/App.java
+++ src/main/java/bioInt/test/App.java
@@ -4,36 +4,37 @@ import org.canova.api.records.reader.RecordReader;
 import org.canova.api.records.reader.impl.CSVRecordReader;
 import org.canova.api.split.FileSplit;
 import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
-import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
 import org.deeplearning4j.nn.conf.distribution.UniformDistribution;
 import org.deeplearning4j.nn.conf.layers.RBM;
 import org.deeplearning4j.nn.conf.override.ClassifierOverride;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
-import java.util.Collections;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
-        RecordReader recordReader = new CSVRecordReader();
-        FileSplit csv = new FileSplit(new ClassPathResource("train.csv").getFile());
-        recordReader.initialize(csv);
-        DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, 10, 0, 2);
+        DataSetIterator trainIterator = getDataSetIterator("train.csv");
+        DataSetIterator validationIterator = getDataSetIterator("validate.csv");
+
+        DataSet trainDataSet = trainIterator.next(10000);
+        DataSet validateDataSet = validationIterator.next(1000);
 
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .layer(new RBM())
-                .nIn(iterator.inputColumns())
-                .nOut(iterator.totalOutcomes())
+                .nIn(trainIterator.inputColumns())
+                .nOut(trainIterator.totalOutcomes())
                 .visibleUnit(RBM.VisibleUnit.GAUSSIAN)
                 .hiddenUnit(RBM.HiddenUnit.RECTIFIED)
                 .weightInit(WeightInit.DISTRIBUTION)
@@ -52,7 +53,18 @@ public class App {
 
         MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.init();
-        model.fit(iterator);
+        model.fit(trainDataSet);
 
+        Evaluation eval = new Evaluation();
+        INDArray predict = model.output(validateDataSet.getFeatureMatrix());
+        eval.eval(validateDataSet.getLabels(), predict);
+        System.out.printf("Score: %s\n", eval.stats());
+    }
+
+    static DataSetIterator getDataSetIterator(String resource) throws IOException, InterruptedException {
+        RecordReader recordReader = new CSVRecordReader();
+        FileSplit csv = new FileSplit(new ClassPathResource(resource).getFile());
+        recordReader.initialize(csv);
+        return new RecordReaderDataSetIterator(recordReader, 10, 0, 2);
     }
 }
