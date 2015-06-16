85702237bd2d50145dcf0797d2d73a466ed328a0
kodelb <boris.kodel@gmail.com>
2015-06-16 15:05:33 +0300

    test

--- src/main/java/bioInt/test/App.java
+++ src/main/java/bioInt/test/App.java
@@ -1,13 +1,26 @@
 package bioInt.test;
 
+import org.canova.api.records.reader.RecordReader;
+import org.canova.api.records.reader.impl.CSVRecordReader;
+import org.canova.api.split.FileSplit;
+import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        RecordReader recordReader = new CSVRecordReader();
+        FileSplit csv = new FileSplit(new ClassPathResource("train.csv").getFile());
+        DataSetIterator iter = new RecordReaderDataSetIterator(recordReader, 2, 0, 2);
+        DataSet next = iter.next();
+        System.out.println(next);
     }
 }
