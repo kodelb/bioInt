cb58c2cd3f076d537b7c4e7054c0bb638aa85924
kodelb <boris.kodel@gmail.com>
2015-06-16 22:22:56 +0300

    add NN

--- src/main/java/bioInt/test/App.java
+++ src/main/java/bioInt/test/App.java
@@ -4,27 +4,55 @@ import org.canova.api.records.reader.RecordReader;
 import org.canova.api.records.reader.impl.CSVRecordReader;
 import org.canova.api.split.FileSplit;
 import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
-import org.nd4j.linalg.dataset.DataSet;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
+import org.deeplearning4j.nn.conf.distribution.UniformDistribution;
+import org.deeplearning4j.nn.conf.layers.RBM;
+import org.deeplearning4j.nn.conf.override.ClassifierOverride;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.util.Collections;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException, InterruptedException {
-        RecordReader recordReader = new CSVRecordReader(0, ",");
-        FileSplit csv = new FileSplit(new ClassPathResource("validate.csv").getFile());
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        RecordReader recordReader = new CSVRecordReader();
+        FileSplit csv = new FileSplit(new ClassPathResource("train.csv").getFile());
         recordReader.initialize(csv);
-        DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, 3, 0, 2);
-        DataSet next = iterator.next();
-        System.out.println(next.numInputs());
-        System.out.println(next.numExamples());
-        System.out.println(next.numOutcomes());
-        System.out.println(next);
+        DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, 10, 0, 2);
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .layer(new RBM())
+                .nIn(iterator.inputColumns())
+                .nOut(iterator.totalOutcomes())
+                .visibleUnit(RBM.VisibleUnit.GAUSSIAN)
+                .hiddenUnit(RBM.HiddenUnit.RECTIFIED)
+                .weightInit(WeightInit.DISTRIBUTION)
+                .dist(new UniformDistribution(0, 1))
+                .constrainGradientToUnitNorm(true)
+                .iterations(5)
+                .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                .learningRate(1e-1f)
+                .momentum(0.9)
+                //.momentumAfter(Collections.singletonMap(3, 0.9))
+                //.optimizationAlgo(OptimizationAlgorithm.CONJUGATE_GRADIENT)
+                .list(2)
+                .hiddenLayerSizes(400)
+                .override(1, new ClassifierOverride())
+                .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        model.fit(iterator);
+
     }
 }
