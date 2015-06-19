a9ca36a91aa9d19f4290c81c8e0a4e37f6996042
kodelb <boris.kodel@gmail.com>
2015-06-19 11:55:19 +0300

    add prediction

--- src/main/java/bioInt/test/App.java
+++ src/main/java/bioInt/test/App.java
@@ -4,8 +4,6 @@ import org.canova.api.records.reader.RecordReader;
 import org.canova.api.records.reader.impl.CSVRecordReader;
 import org.canova.api.split.FileSplit;
 import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
-import org.deeplearning4j.datasets.iterator.SamplingDataSetIterator;
-import org.deeplearning4j.datasets.test.TestDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -20,7 +18,13 @@ import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Calendar;
 
 /**
  * Hello world!
@@ -29,6 +33,7 @@ public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
         DataSetIterator trainIterator = getDataSetIterator("train.csv");
         DataSetIterator validationIterator = getDataSetIterator("validate.csv");
+        DataSetIterator testIterator = getDataSetIterator("test.csv");
 
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .layer(new RBM())
@@ -48,7 +53,7 @@ public class App {
                 //.optimizationAlgo(OptimizationAlgorithm.CONJUGATE_GRADIENT)
                 .list(2)
                 .hiddenLayerSizes(100)
-                .useDropConnect(true)
+                //.useDropConnect(true)
                 .override(1, new ClassifierOverride())
                 .build();
 
@@ -66,6 +71,21 @@ public class App {
         }
 
         System.out.printf("Score: %s\n", eval.stats());
+
+        System.out.println(conf.toJson());
+
+        Path newFile = Paths.get(Double.toString(eval.accuracy()));
+        try(BufferedWriter writer = Files.newBufferedWriter(
+                newFile, Charset.defaultCharset())){
+            while(testIterator.hasNext()) {
+                DataSet test = testIterator.next();
+                INDArray predict = model.output(test.getFeatureMatrix());
+                for(int i = 0; i < predict.rows(); i++)
+                {
+                    writer.write(predict.getRow(i).toString().trim());
+                }
+            }
+        }
     }
 
     static DataSetIterator getDataSetIterator(String resource) throws IOException, InterruptedException {
